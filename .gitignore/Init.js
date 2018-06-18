2f90f66ad3b6b45b822ed3cb4b93e2ecaaced4c3
fabuss254 <fabuss254@outlook.fr>
2018-06-18 21:48:56 +0200

    Update Init.js

--- .gitignore/Init.js
+++ .gitignore/Init.js
@@ -1,6 +1,7 @@
 /* VARIABLES */
 
 const Discord = require("discord.js");
+const gd = require('node-gd');
 
 var prefix = "%";
 var footer = "Created by Fabuss254#9232";
@@ -11,6 +12,35 @@ var bot = new Discord.Client();
 
 bot.on('ready', () => {
   console.log("bot ready")
+});
+
+bot.on('message', message => {
+  if (message.author.equals(bot.user)) return;
+  var args = message.content.substring(prefix.length).split (" ");
+  if (!message.content.startsWith(prefix)) return;
+  switch (args[0].toLowerCase()) {
+    
+    case "canvas1":
+      var img = gd.createSync(200, 80);
+      img.colorAllocate(0, 255, 0);
+      var txtColor = img.colorAllocate(255, 0, 255);
+      var fontPath = '/Fonts/Ezo.ttf';
+      img.stringFT(txtColor, fontPath, 24, 0, 10, 60, 'Hello world!');
+      img.savePng('output.png', 1, function(err) {
+        if(err) {
+          throw err;
+        }
+        message.send({files: [{attachment: 'output.png',name: 'output.png'}]})
+      });
+
+      // Destroy image to clean memory
+      img.destroy();
+      
+    default:
+      message.channel.send("Invalid command!").delete(5000);
+      message.delete(100);
+    break;
+  }
 });
 
 /* FUNCTION */
